d04c33134e322c2d2d50423726284b92964e86bc
Cassandra Dunlop <cdulop@columbia.com>
2015-06-02 17:46:34 -0400

    add csrf handling js from django site

--- media/js/uelc_admin/group_js.js
+++ media/js/uelc_admin/group_js.js
@@ -1,5 +1,39 @@
 $(function() {
 
+	/* Adding csrf handling JS from Django site */
+	
+	// using jQuery
+	function getCookie(name) {
+	    var cookieValue = null;
+	    if (document.cookie && document.cookie != '') {
+	        var cookies = document.cookie.split(';');
+	        for (var i = 0; i < cookies.length; i++) {
+	            var cookie = jQuery.trim(cookies[i]);
+	            // Does this cookie string begin with the name we want?
+	            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+	                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+	                break;
+	            }
+	        }
+	    }
+	    return cookieValue;
+	}
+	
+	var csrftoken = getCookie('csrftoken');
+	
+	function csrfSafeMethod(method) {
+	    // these HTTP methods do not require CSRF protection
+	    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	
+	$.ajaxSetup({
+	    beforeSend: function(xhr, settings) {
+	        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+	            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+	        }
+	    }
+	});
+	
 	$('button.curveball-btn').on('click', function () {
 		//console.log("group has clicked on a curveball button");
 		//alert("group has clicked on a curveball button");
